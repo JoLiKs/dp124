606da4e0134c770e05da5310332738a877b96764
Ramosok <Ramosok86@gmail.com>
2023-01-27 16:49:20 +0300

    add new functional

--- src/App/App.js
+++ src/App/App.js
@@ -3,18 +3,11 @@ import { MainFirstScreen } from '../Features/MainFirstScreen/MainFirstScreen';
 import { AccordionComponent } from '../Features/AccordionComponent/AccordionComponent';
 import { PhotoIcon } from '../Shared';
 import styles from './App.module.scss';
-import { Tabs } from '../Shared/Tabs/Tabs';
 
 export const App = () => {
-    const items = [
-        { tabName: 'Модуль 1', title: 'London', content: 'London is the capital city of England.' },
-        { tabName: 'Модуль 2', title: 'Paris', content: 'Paris is the capital of France.' },
-        { tabName: 'Модуль 3', title: 'Tokyo', content: 'Tokyo is the capital of Japan.' },
-    ];
     return (
         <main className={styles.layout}>
             <Header />
-            <Tabs items={items} />
             <MainFirstScreen />
             <PhotoIcon />
             <AccordionComponent />
