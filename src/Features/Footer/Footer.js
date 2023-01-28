593cda17fa838cd1e5fdd413261d69206a963cb8
Vitaliy <118274433+vitaliy-maslionik-idf@users.noreply.github.com>
2023-01-28 10:32:26 +0300

    Vit dev

--- src/Features/Footer/Footer.js
+++ src/Features/Footer/Footer.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import styles from './Footer.module.scss';
+import { coursesList, LogoIcon, navBtnList } from '../../Shared';
+import { Social } from '../Header/Social';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
-    return <footer className={styles.footer}></footer>;
+    return (
+        <footer className={styles.footer}>
+            <div>
+                <a href="#">Договор публичной оферты</a>
+                <ul>
+                    <li>{navBtnList[0].btnName}</li>
+                    {coursesList.map(({ courseName }) => (
+                        <li>{courseName}</li>
+                    ))}
+                    <li>{navBtnList[1].btnName}</li>
+                </ul>
+            </div>
+            <div>
+                <Social />
+            </div>
+            <div>
+                <Link to="/">
+                    <LogoIcon style={{ color: 'red' }} />
+                </Link>
+                <p>© 2023 Все права защищены.</p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
