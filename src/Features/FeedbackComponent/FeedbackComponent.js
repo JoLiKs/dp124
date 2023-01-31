9d67996cf2c38568a43fbc7688ff345a0d0501c8
Vitaliy <118274433+vitaliy-maslionik-idf@users.noreply.github.com>
2023-01-31 09:31:52 +0300

    add new functional

--- src/Features/FeedbackComponent/FeedbackComponent.js
+++ src/Features/FeedbackComponent/FeedbackComponent.js
@@ -3,47 +3,15 @@ import { BurstStarIcon, Button, CheckBox, Input } from '../../Shared';
 
 const FeedbackComponent = () => {
     const [name, setName] = useState('');
+    const [phoneOrEmail, setPhoneOrEmail] = useState('');
     const [comment, setComment] = useState('');
     const [checkbox, setCheckbox] = useState(false);
 
-    const disabled = !name && !comment && !checkbox;
+    const disabled = !name && !phoneOrEmail && !checkbox;
 
     const handleSubmit = (event) => {
         event.preventDefault();
     };
-
-    // function setCursorPosition(pos, elem) {
-    //     elem.focus();
-    //
-    //     if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
-    //     else if (elem.createTextRange) {
-    //         var range = elem.createTextRange();
-    //
-    //         range.collapse(true);
-    //
-    //         range.moveEnd('character', pos);
-    //
-    //         range.moveStart('character', pos);
-    //
-    //         range.select();
-    //     }
-    // }
-    // function mask(event) {
-    //     var matrix = '+7 (___) ___ ____',
-    //         i = 0,
-    //         def = matrix.replace(/\D/g, ''),
-    //         val = this.value.replace(/\D/g, '');
-    //
-    //     if (def.length >= val.length) val = def;
-    //
-    //     this.value = matrix.replace(/./g, function (a) {
-    //         return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
-    //     });
-    //
-    //     if (event.type == 'blur') {
-    //         if (this.value.length == 2) this.value = '';
-    //     } else setCursorPosition(this.value.length, this);
-    // }
 
     return (
         <div>
@@ -56,6 +24,13 @@ const FeedbackComponent = () => {
             </p>
             <form onSubmit={handleSubmit}>
                 <Input type="text" legend="Имя*" placeholder="Введите ваше имя" value={name} onChange={setName} />
+                <Input
+                    type="text"
+                    legend="Номер или E-mail*"
+                    placeholder="Введите Номер или E-mail"
+                    value={phoneOrEmail}
+                    onChange={setPhoneOrEmail}
+                />
                 <Input
                     type="text"
                     legend="Комментарий"
