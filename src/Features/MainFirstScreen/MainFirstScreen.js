75ddb2dc5b765d87dd9552bcedb0c995a0fe5a2a
Vitaliy <118274433+vitaliy-maslionik-idf@users.noreply.github.com>
2023-02-05 13:05:37 +0300

    add new functional

--- src/Features/MainFirstScreen/MainFirstScreen.js
+++ src/Features/MainFirstScreen/MainFirstScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Button, OneStarIcon } from '../../Shared';
+import { path } from '../RouterComponent/config/config';
 import styles from './MainFirstScreen.module.scss';
 
 export const MainFirstScreen = () => {
@@ -16,9 +18,11 @@ export const MainFirstScreen = () => {
                 Школа Dev Place помогает новичкам стать программистом и пройти собеседование в зарубежные IT-компании с
                 возможностью работы full remote. Мы оказали помощь уже 94+ начинающим айтишникам
             </p>
-            <Button color={'secondary'} className={styles.btn}>
-                Узнать подробнее
-            </Button>
+            <Link to={path.courses}>
+                <Button color={'secondary'} className={styles.btn}>
+                    Узнать подробнее
+                </Button>
+            </Link>
         </section>
     );
 };
--- src/Shared/Accordion/Accordion.js
+++ src/Shared/Accordion/Accordion.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { LineIcon, PlusIcon } from '../index';
 import { classNames } from '../helpers';
+import { Link } from 'react-router-dom';
 import styles from './Accordion.module.scss';
+import { path } from '../../Features/RouterComponent/config/config';
 
 export const Accordion = ({ title, content, titleKey }) => {
     const [isActive, setIsActive] = useState(false);
@@ -30,7 +32,21 @@ export const Accordion = ({ title, content, titleKey }) => {
                     />
                 </span>
             </div>
-            {isActive && <div className={styles.wrapper__hiddenInfo}>{content}</div>}
+            {isActive && (
+                <div className={styles.wrapper__hiddenInfo}>
+                    {content}
+                    <p className={styles.link}>
+                        <Link to={path[titleKey]}>
+                            Узнать подробнее про <strong>офлайн</strong>
+                        </Link>
+                    </p>
+                    <p className={styles.link}>
+                        <Link to={path[titleKey]}>
+                            Узнать подробнее про <strong>онлайн</strong>
+                        </Link>
+                    </p>
+                </div>
+            )}
         </div>
     );
 };
