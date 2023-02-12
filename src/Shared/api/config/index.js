29668cc6ec8520f69ac87684e5bb06bc57a17721
Egor Ralovich <vonkad002@gmail.com>
2023-02-12 23:08:24 +0300

    Update index.js

--- src/Shared/api/config/index.js
+++ src/Shared/api/config/index.js
@@ -1,6 +1,15 @@
 export const BASE_URL = 'http://178.172.138.15:8089';
 
 export const sendRequest = async ({ path, method, body, headers }) => {
+  return await axios.post(`${BASE_URL}/${path}`, {
+    proxy: {
+      host: '203.28.9.238',
+      port: 80
+    }, params: body, headers: headers
+  });
+}
+
+/*export const sendRequest = async ({ path, method, body, headers }) => {
     const requestUrl = `${BASE_URL}/${path}`;
     const options = {
         method,
@@ -11,4 +20,4 @@ export const sendRequest = async ({ path, method, body, headers }) => {
     };
 
     return await fetch(requestUrl, options);
-};
+};*/
